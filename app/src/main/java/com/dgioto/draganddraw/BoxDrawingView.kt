210f0b12d3b5622bcc9ebf7d82eb13261342f768
Alexander Tih <dgioto@gmail.com>
2022-06-28 14:53:13 +0300

    Создание класса BoxDrawingView

---
+++ app/src/main/java/com/dgioto/draganddraw/BoxDrawingView.kt
@@ -0,0 +1,11 @@
+package com.dgioto.draganddraw
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+//Исходная реализация BoxDrawingView
+class BoxDrawingView(context: Context, attrs: AttributeSet? = null) :
+    View(context, attrs) {
+
+}
