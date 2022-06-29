ff49930c22349d33f42d631302aebf7e932f4b54
Alexander Tih <dgioto@gmail.com>
2022-06-29 09:34:35 +0300

    Отслеживание событий перемещения

--- app/src/main/java/com/dgioto/draganddraw/BoxDrawingView.kt
+++ app/src/main/java/com/dgioto/draganddraw/BoxDrawingView.kt
@@ -13,6 +13,9 @@ private const val TAG = "BoxDrawingView"
 class BoxDrawingView(context: Context, attrs: AttributeSet? = null) :
     View(context, attrs) {
 
+    private var currentBox: Box? = null
+    private val boxen = mutableListOf<Box>()
+
     //Реализация BoxDrawingView
     override fun onTouchEvent(event: MotionEvent): Boolean {
         val current = PointF(event.x, event.y)
@@ -21,19 +24,35 @@ class BoxDrawingView(context: Context, attrs: AttributeSet? = null) :
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
                 action = "ACTION_DOWN"
+                // Сбросить состояние объекта
+                currentBox = Box(current).also {
+                    boxen.add(it)
+                }
             }
             MotionEvent.ACTION_MOVE -> {
                 action = "ACTION_MOVE"
+                updateCurrentBox(current)
             }
             MotionEvent.ACTION_UP -> {
                 action = "ACTION_UP"
+                updateCurrentBox(current)
+                currentBox = null
             }
             MotionEvent.ACTION_CANCEL -> {
                 action = "ACTION_CANCEL"
+                currentBox = null
             }
         }
 
         Log.i(TAG, "$action at x=${current.x}, y=${current.y}")
         return true
+    }
+
+    //Добавление функций жизненного цикла событий касания
+    private fun updateCurrentBox(current: PointF) {
+        currentBox?.let {
+            it.end = current
+            invalidate()
+        }
     }
 }
